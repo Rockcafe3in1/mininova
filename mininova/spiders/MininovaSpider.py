5b585747ed14ab614db7fc7b6fdd5321a3c8ee20
rockcafe <Liang7Zhang@gmail.com>
2015-07-11 11:30:16 +0800

    Initial commit

---
+++ mininova/spiders/MininovaSpider.py
@@ -0,0 +1,20 @@
+__author__ = 'liang'
+
+from mininova.item import TorrentItem
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+
+class MininovaSpider(CrawlSpider):
+
+    name = 'mininova'
+    allowed_domains = ['mininova.org']
+    start_urls = ['http://www.mininova.org/tor/2676093']
+    rules = [Rule(LinkExtractor(allow=['/tor/\d+']), 'parse_torrent')]
+
+    def parse_torrent(self, response):
+        torrent = TorrentItem()
+        torrent['url'] = response.url
+        torrent['name'] = response.xpath("//div[@id='content']/h1/text()").extract()
+        torrent['description'] = response.xpath("//div[@id='content']/div[@id='description']/text()").extract()
+        torrent['size'] = response.xpath("//div[@id='content']/div[@id='specifications']/p[2]/text()[2]").extract()
+        return torrent
